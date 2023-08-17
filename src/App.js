76573e7b11cd5eb0792864804a5fb8c0640fcb22
JJ_Falcone <jorgefalcone02@gmail.com>
2023-08-16 22:05:18 -0300

    finalizado

--- src/App.js
+++ src/App.js
@@ -5,51 +5,18 @@ import Footer from './componentes/Footer';
 import { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
+
 function App() {
 
   const colaboradoresRecuperados = JSON.parse(localStorage.getItem('colaboradores')) === null ? [] : JSON.parse(localStorage.getItem('colaboradores'))
-
-  const [times, setTimes] = useState([
-    {
-      id: uuidv4(),
-      nome: 'Programação',
-      cor: '#57C278',
-    },
-    {
-      id: uuidv4(),
-      nome: 'Front-End',
-      cor: '#82CFFA',
-    },
-    {
-      id: uuidv4(),
-      nome: 'Data Science',
-      cor: '#A6D157',
-    },
-    {
-      id: uuidv4(),
-      nome: 'Devops',
-      cor: '#E06B69',
-    },
-    {
-      id: uuidv4(),
-      nome: 'UX e Design',
-      cor: '#DB6EBF',
-    },
-    {
-      id: uuidv4(),
-      nome: 'Mobile',
-      cor: '#FFBA05',
-    },
-    {
-      id: uuidv4(),
-      nome: 'Inovação e gestão',
-      cor: '#FF8A29',
-    }
-
-  ])
+  const timesRecuperados = JSON.parse(localStorage.getItem('times')) === null ? [] : JSON.parse(localStorage.getItem('times'))
 
   const [colaboradores, setColaboradores] = useState(colaboradoresRecuperados)
+  const [times, setTimes] = useState(timesRecuperados)
+
   let colaboradoresTemporarios = colaboradoresRecuperados
+  let timesTemporarios = times
+
 
   const aoNovoColaboradorAdicionado = (colaborador) => {
     colaborador.id = uuidv4()
@@ -68,18 +35,25 @@ function App() {
     setTimes(times.map(time => {
       if (time.id === id) {
         time.cor = cor;
+        timesTemporarios = times
+        localStorage.setItem('times', JSON.stringify(timesTemporarios))
       }
       return time
     }))
   }
 
   function cadastrarTime(novoTime) {
-    setTimes([...times, {...novoTime, id: uuidv4()}])
+    const idtemp = uuidv4()
+    setTimes([...times, {...novoTime, id: idtemp}])
+    timesTemporarios = [...times, {...novoTime, id: idtemp}]
+    localStorage.setItem('times', JSON.stringify(timesTemporarios))
   }
 
-  function resolverFavorito(id) {
+  function resolverFavorito(id) { 
     setColaboradores(colaboradores.map(colaborador => {
       if(colaborador.id === id)colaborador.favorito = !colaborador.favorito;
+      colaboradoresTemporarios = colaboradores
+      localStorage.setItem('colaboradores', JSON.stringify(colaboradoresTemporarios))
       return colaborador
     }))
   }
